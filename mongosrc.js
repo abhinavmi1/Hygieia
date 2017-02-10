9733fe80ac1caaee919f7bea7e1b6c21d2f33a4c
abhinavmi1 <abhinav.mishra@accenture.com>
2017-02-10 23:35:38 +0000

    Working code

---
+++ mongosrc.js
@@ -0,0 +1,10 @@
+use dashboarddb
+db.createUser(
+    {
+        user: "dashboarduser",
+        pwd: "1qazxSw2",
+        roles: [
+            {role: "readWrite", db: "dashboard"}
+        ]
+    })
+db.dummmyCollection.insert({x: 1});
